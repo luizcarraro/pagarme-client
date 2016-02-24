4736f543d629e9a65925529a164f6379d90f0899
Luiz Carraro <lacarraro@gmail.com>
2016-02-24 13:56:57 -0300

    Creates a transaction test file, with to-do data

---
+++ test/transaction.test.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var should = require('should'),
+  helper = require('./helper'),
+  Transaction = helper.pagarMe.Transaction;
+
+var info = {};
+
+describe('Customer', function() {
+  it('should be able to create a transaction', function(done) {
+    console.log('TODO');
+    done()
+  });
+
+  it('should update a transaction', function(done) {
+    console.log('TODO');
+    done()
+  });  
+
+  it('should be able to return a transaction by id', function(done) {
+    console.log('TODO');
+    done()
+  });
+
+  it('should be able to return all transactions', function(done) {
+    console.log('TODO');
+    done()
+  });
+
+  it('should find all spit rules', function(done) {
+    console.log('TODO');
+    done()
+  });
+
+  it('should find a split rule by id', function(done) {
+    console.log('TODO');
+    done()
+  });
+
+  it('should find all payables of a transaction', function(done) {
+    console.log('TODO');
+    done()
+  });
+});
