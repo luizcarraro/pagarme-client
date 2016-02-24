f252d9e84ca2693593d369df2f986e8f75d579f0
Luiz Carraro <lacarraro@gmail.com>
2016-02-24 11:45:46 -0300

    format file

--- test/helper.js
+++ test/helper.js
@@ -1,30 +1,29 @@
-
 var chance = require('chance')();
 
 module.exports = {
-	pagarMe: require('../index')('*****'),
+    pagarMe: require('../index')('****'),
 
-	customer: { 
-    document_number: "61842181408",
-    document_type: 'cpf',
-    name: chance.name(),
-    email: chance.email(),
-    born_at: chance.birthday(),
-    gender: "M",
-    phones: [{
-        ddi: "55",
-        ddd: "11",
-        number: "999887766",
-    }],
-    addresses: [{
-        street: chance.word(),
-        complementary: "1",
-        street_number: "2",
-        neighborhood: "none",
-        city: chance.city(),
-        state: "PR",
-        zipcode: "8546000",
-        country: "Brasil",
-    }]
-}
+    customer: {
+        document_number: "61842181408",
+        document_type: 'cpf',
+        name: chance.name(),
+        email: chance.email(),
+        born_at: chance.birthday(),
+        gender: "M",
+        phones: [{
+            ddi: "55",
+            ddd: "11",
+            number: "999887766",
+        }],
+        addresses: [{
+            street: chance.word(),
+            complementary: "1",
+            street_number: "2",
+            neighborhood: "none",
+            city: chance.city(),
+            state: "PR",
+            zipcode: "8546000",
+            country: "Brasil",
+        }]
+    }
 };
