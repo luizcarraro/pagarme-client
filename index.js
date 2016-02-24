51ef4a23005d65494befd2f06535f35c89ea0d77
Luiz Carraro <lacarraro@gmail.com>
2016-02-24 11:47:05 -0300

    Add verification for missing api_key and future module references

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var modelFunctions = {
-  card          = require('./lib/card.js'),
-  customer      = require('./lib/customer.js'),
-  plan          = require('./lib/plan.js'),
-  subscription  = require('./lib/subscription.js'),
-  payable       = require('./lib/payable.js'),
-  balance       = require('./lib/balance.js'),
-  bank_account  = require('./lib/bank_account.js'),
-  recipient     = require('./lib/recipient.js'),
-  transfer      = require('./lib/transfer.js'),
-  transaction   = require('./lib/transaction.js'),
-  zipcode       = require('./lib/zipcode.js'),
-  fingerprint   = require('./lib/fingerprint.js')
+  // card          : require('./lib/card.js'),
+  customer      : require('./lib/customer.js')
+  // plan          : require('./lib/plan.js'),
+  // subscription  : require('./lib/subscription.js'),
+  // payable       : require('./lib/payable.js'),
+  // balance       : require('./lib/balance.js'),
+  // bank_account  : require('./lib/bank_account.js'),
+  // recipient     : require('./lib/recipient.js'),
+  // transfer      : require('./lib/transfer.js'),
+  // transaction   : require('./lib/transaction.js'),
+  // zipcode       : require('./lib/zipcode.js'),
+  // fingerprint   : require('./lib/fingerprint.js')
 }
 
 function PagarmeClient (key) {
+  if(!key) throw { name: 'MissingKey', message: 'A chave do pagarme está faltando'};
   if (!(this instanceof PagarmeClient)) return new PagarmeClient(key);
 
   this.api = {
