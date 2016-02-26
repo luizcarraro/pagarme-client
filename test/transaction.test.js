202e0406ed7d55098b8afca60f68b65cb57640a0
Luiz Carraro <lacarraro@gmail.com>
2016-02-26 13:36:25 -0300

    new tests for transactions

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -15,7 +15,6 @@ describe.only('Transaction', function() {
       Transaction
         .create(transaction)
         .then(function(response) {
-          console.log(response);
           response.should.be.ok;
           info.transaction = response;
           done();
@@ -24,37 +23,68 @@ describe.only('Transaction', function() {
   });
 
   it('should not be able to update a transaction', function(done) {
-    Transaction.update(info.transaction.id,{capture: true}).then(function (response) {
-      done('should not be abe to update');
-    })
-    .catch(function (error) {
-      should.exist.error;
-      done();
-    })
-  });  
+    Transaction
+      .update(info.transaction.id, {
+        capture: true
+      }).then(function(response) {
+        done('should not be abe to update');
+      })
+      .catch(function(error) {
+        should.exist.error;
+        done();
+      })
+  });
 
-  // it('should be able to return a transaction by id', function(done) {
-  //   console.log('TODO');
-  //   done()
-  // });
+  it('should capture a transaction that was holding', function(done) {
+    Transaction
+      .capture(info.transaction.id)
+      .then(function(response) {
+        response.status.should.be.equal('authorized');
+        done();
+      });
+  });
 
-  // it('should be able to return all transactions', function(done) {
-  //   console.log('TODO');
-  //   done()
-  // });
+  it('should be able to return a transaction by id', function(done) {
+    Transaction
+      .findById(info.transaction.id)
+      .then(function(response) {
+        response.should.be.ok;
+        should.exist(response.id);
+        response.object.should.be.equal('transaction');
+        done();
+      });
+  });
 
-  // it('should find all spit rules', function(done) {
-  //   console.log('TODO');
-  //   done()
-  // });
+  it('should be able to return all transactions', function(done) {
+    Transaction
+      .findAll()
+      .then(function(response) {
+        response.should.be.ok;
+        response.should.be.instanceof(Array);
+        done();
+      });
+  });
 
-  // it('should find a split rule by id', function(done) {
-  //   console.log('TODO');
-  //   done()
-  // });
+  it('should find all split rules', function(done) {
+    Transaction.findAllSplitRules(info.transaction.id)
+      .then(function(response) {
+        response.should.be.ok;
+        response.should.be.instanceof(Array);
+        done();
+      });
+  });
 
-  // it('should find all payables of a transaction', function(done) {
-  //   console.log('TODO');
-  //   done()
-  // });
+  it('should find a split rule by id', function(done) {
+    console.log('            TODO');
+    done()
+  });
+
+  it('should find all payables of a transaction', function(done) {
+   Transaction.findAllPayables(info.transaction.id)
+      .then(function(response) {
+        response.should.be.ok;
+        response.should.be.instanceof(Array);
+        done();
+      });
+  });
 });
