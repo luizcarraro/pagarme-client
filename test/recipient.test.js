0d549c668a610d49281ee7e1762bb218e3955d8f
Luiz Carraro <lacarraro@gmail.com>
2016-02-27 22:40:39 -0300

    declare new tests with TODO flag

--- test/recipient.test.js
+++ test/recipient.test.js
@@ -9,10 +9,34 @@ var info = {};
 describe.only('Recipient', function() {
   it('should be able to create a recipient', function(done) {
     Recipient.create(helper.recipient)
-      .then(function (response) {
+      .then(function(response) {
         response.should.be.ok;
         done();
       });
   });
 
+  it('should update a recipient', function(done) {
+    console.log('   TODO');
+    done();
+  });
+
+  it('should return all recipients', function(done) {
+    console.log('   TODO');
+    done();
+  });
+
+  it('should return a recipient balance', function(done) {
+    console.log('    TODO');
+    done();
+  });
+
+  it('should return a list of recipient operations', function(done) {
+    console.log('    TODO');
+    done();
+  });
+
+  it('should return a especififc recipient operation by id', function(done) {
+    console.log('    TODO');
+    done();
+  });
 });
