99609f897761c84e7513c8213a6efc15dd525191
Luiz Carraro <lacarraro@gmail.com>
2016-04-04 17:49:53 -0300

    add bank account lib

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var models = {
+  bankAccount   : require('./lib/bank_account.js'),
   customer      : require('./lib/customer.js'),
   transaction   : require('./lib/transaction.js'),
   recipient     : require('./lib/recipient.js')
