617e3571e5963b5dc9124d58886168bc057af75a
Luiz Carraro <lacarraro@gmail.com>
2016-04-20 21:45:15 -0300

    Add test file for Subscription

---
+++ test/subscription.test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var should = require('should');
+var helper = require('./helper');
+var subscription = helper.pagarMe.subscription;
+var _ = require('lodash');
+var info = {};
+
+describe('Subscription', function() {
+  it('should create a new subscription', function(done) {
+    console.log('   TODO');
+    done();
+  });
+});
