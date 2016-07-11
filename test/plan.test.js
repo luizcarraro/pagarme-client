37c63780d5707b8ab95f2a2c9cdd8a0661c185c5
Luiz Carraro <lacarraro@gmail.com>
2016-07-11 18:31:57 -0300

    Add new test for Plan model

---
+++ test/plan.test.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var should = require('should');
+var helper = require('./helper');
+var Card = helper.pagarMe.plan;
+var _ = require('lodash');
+var info = {};
+
+describe('#Plan', function() {
+  it('should create a plan', function(done) {
+    console.log('			TODO');
+    done();
+  });
+
+  it('should return all plans', function (done) {
+    console.log(' 	  TODO');
+    done();  	
+  });
+
+  it('should return a plan', function(done) {
+    console.log('	   TODO');
+    done();
+  });
+});
